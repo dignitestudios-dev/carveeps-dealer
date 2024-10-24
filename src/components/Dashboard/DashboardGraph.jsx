7cf4499efb3959fdfbf8d8954312d31849548cf8
JackAnderson01 <120801517+Hammadulhaq123@users.noreply.github.com>
2024-10-24 13:16:49 +0500

    Graph code completed

--- src/components/Dashboard/DashboardGraph.jsx
+++ src/components/Dashboard/DashboardGraph.jsx
@@ -55,55 +55,25 @@ const options = {
   },
 };
 
-function getMonthsArray() {
-  const monthsArray = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  return monthsArray;
-}
-
-function getRandomDataArray() {
-  // Generate an array with random data for 12 months
-  const randomDataArray = Array.from(
-    { length: 12 },
-    () => Math.floor(Math.random() * 100) + 1
-  );
-
-  return randomDataArray;
-}
+const monthsArray = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const DashboardGraph = () => {
   const [showOpt3, setShowOpt3] = useState(false);
   const [showOpt4, setShowOpt4] = useState(false);
 
-  const handlePersonChange = (event, personId) => {
-    if (event.target.checked) {
-      if (selectedPersons?.length < 2) {
-        // Add plan ID to the array
-        setSelectedPersons([...selectedPersons, personId]);
-      } else {
-        // Prevent selecting more than two plans
-        event.preventDefault();
-
-        setError("You can only select up to two salesperson.");
-      }
-    } else {
-      // Remove plan ID from the array
-      setSelectedPersons(selectedPersons.filter((id) => id !== personId));
-    }
-  };
   const [openDropdown2, setOpenDropdown2] = useState(false);
   const [openDropdown3, setOpenDropdown3] = useState(false);
   const toggleDropdown2 = () => {
@@ -117,9 +87,18 @@ const DashboardGraph = () => {
   const [filter2, setFilter2] = useState("yearly");
 
   const { team, setError, baseUrl, setNewUpdate } = useContext(GlobalContext);
-  const [selectedPersons, setSelectedPersons] = useState(
-    team?.length > 0 ? [team[0]?._id] : []
-  );
+
+  const currentYear = new Date().getFullYear();
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const getYearsArray = () => {
+    const startYear = 2024;
+    const years = [];
+    for (let year = startYear; year <= currentYear; year++) {
+      years.push(year);
+    }
+    return years;
+  };
 
   const [personData, setPersonData] = useState([]);
   const [personDataLoading, setPersonDataLoading] = useState(false);
@@ -132,13 +111,13 @@ const DashboardGraph = () => {
       };
       setPersonDataLoading(true);
       axios
-        .post(
-          `${baseUrl}/dealership/dashboard/graph`,
+        .get(
+          selectedMonth == null
+            ? `${baseUrl}/dealership/dashboard/graph?year=${selectedYear}`
+            : `${baseUrl}/dealership/dashboard/graph?year=${selectedYear}&month=${selectedMonth?.number}`,
           {
-            salesPersons: selectedPersons,
-            filter: filter2,
-          },
-          { headers }
+            headers,
+          }
         )
         .then((response) => {
           setPersonData(response?.data?.data);
@@ -152,52 +131,23 @@ const DashboardGraph = () => {
   };
 
   useEffect(() => {
-    selectedPersons?.length > 0 && getPersonData();
-  }, [selectedPersons, filter2]);
-
-  useEffect(() => {
-    setNewUpdate((prev) => !prev);
-  }, []);
+    getPersonData();
+  }, [selectedYear, selectedMonth]);
 
   // Sample data
   const data = {
-    labels: personData?.labels,
-
-    datasets:
-      personData?.datasets?.length > 1
-        ? [
-            {
-              label: personData?.datasets[0]?.label,
-              data: personData?.datasets[0]?.data,
-              backgroundColor: "#FF204E",
-              borderColor: "#FF204E",
-              borderWidth: 1,
-              borderRadius: { topLeft: 50, topRight: 50 },
-              borderSkipped: "bottom",
-            },
-            {
-              label: personData?.datasets[1]?.label,
-              data: personData?.datasets[1]?.data,
-              backgroundColor: "#007BFF",
-              borderColor: "#007BFF",
-              borderWidth: 1,
-              borderRadius: { topLeft: 50, topRight: 50 },
-              borderSkipped: "bottom",
-            },
-          ]
-        : personData?.datasets?.length > 0
-        ? [
-            {
-              label: personData?.datasets[0]?.label,
-              data: personData?.datasets[0]?.data,
-              backgroundColor: "#FF204E",
-              borderColor: "#FF204E",
-              borderWidth: 1,
-              borderRadius: { topLeft: 50, topRight: 50 },
-              borderSkipped: "bottom",
-            },
-          ]
-        : [],
+    labels: personData?.result, // This will be your months
+    datasets: [
+      {
+        label: "Subscription Counts", // Dataset label
+        data: personData?.counts, // Subscription counts
+        backgroundColor: "#FF204E", // Bar color
+        borderColor: "#FF204E", // Border color
+        borderWidth: 1,
+        borderRadius: { topLeft: 50, topRight: 50 },
+        borderSkipped: "bottom",
+      },
+    ],
   };
   return (
     <div className="bg-white p-6 mt-4 rounded-[18px] flex flex-col gap-y-6">
@@ -206,10 +156,10 @@ const DashboardGraph = () => {
         <div className="relative flex gap-2">
           <div className="relative z-[1000]">
             <button
-              className="w-[144px]  py-2 flex items-center justify-center gap-[2px] bg-[#EDEDED] rounded-full text-[11px] font-medium"
+              className="w-[124px]  py-2 flex items-center justify-center gap-[2px] bg-[#EDEDED] rounded-full text-[11px] font-medium"
               onClick={toggleDropdown3}
             >
-              Select Salesperson{" "}
+              Select Month{" "}
               {openDropdown3 ? (
                 <IoMdArrowDropdown className="text-base" />
               ) : (
@@ -217,22 +167,24 @@ const DashboardGraph = () => {
               )}
             </button>
             {openDropdown3 && (
-              <div className="bg-white w-[164px] max-h-[200px] overflow-y-scroll modal-scroll custom-shadow absolute flex flex-col items-start gap-3 px-3 py-2">
-                {team?.map((person, key) => {
+              <div className="bg-white w-[124px] max-h-[200px] overflow-y-scroll modal-scroll custom-shadow absolute flex flex-col items-start gap-3 px-3 py-2">
+                {monthsArray?.map((month, key) => {
                   return (
-                    <div key={person?._id} class="flex items-center px-2">
+                    <div key={key} class="flex items-center px-2">
                       <input
                         id="checked-checkbox"
-                        checked={selectedPersons?.includes(person?._id)}
+                        checked={selectedMonth?.name == month}
                         type="checkbox"
-                        onChange={(e) => handlePersonChange(e, person?._id)}
+                        onChange={(e) =>
+                          setSelectedMonth({ name: month, number: key + 1 })
+                        }
                         className="w-3 h-3 text-[#FF204E] rounded dark:focus:[#FF204E] accent-[#FF204E]"
                       />
                       <label
                         for="checked-checkbox"
                         class="ms-2 font-medium text-[11px] text-gray-900 dark:text-gray-300"
                       >
-                        {person?.name}
+                        {month}
                       </label>
                     </div>
                   );
@@ -245,11 +197,7 @@ const DashboardGraph = () => {
               className="w-[86px] z-[100000000] py-2 flex items-center capitalize justify-center gap-[2px] bg-[#EDEDED] rounded-full text-[11px] font-medium"
               onClick={toggleDropdown2}
             >
-              {filter2 == "thisYear"
-                ? "This Year"
-                : filter2 == "last7Days"
-                ? "Last 7 days"
-                : filter2}
+              {selectedYear}
               {openDropdown2 ? (
                 <IoMdArrowDropdown className="text-base" />
               ) : (
@@ -258,51 +206,19 @@ const DashboardGraph = () => {
             </button>
             {openDropdown2 && (
               <div className="bg-white w-[92px] h-auto py-1 custom-shadow absolute flex flex-col items-start">
-                {/* <button
-                  onClick={() => {
-                    toggleDropdown2();
-                    setFilter2("thisYear");
-                  }}
-                  className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
-                >
-                  This Year
-                </button> */}
-                <button
-                  onClick={() => {
-                    toggleDropdown2();
-                    setFilter2("last7Days");
-                  }}
-                  className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
-                >
-                  Last 7 days
-                </button>
-                <button
-                  onClick={() => {
-                    toggleDropdown2();
-                    setFilter2("weekly");
-                  }}
-                  className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
-                >
-                  Weekly
-                </button>
-                <button
-                  onClick={() => {
-                    toggleDropdown2();
-                    setFilter2("monthly");
-                  }}
-                  className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
-                >
-                  Monthly
-                </button>
-                <button
-                  onClick={() => {
-                    toggleDropdown2();
-                    setFilter2("yearly");
-                  }}
-                  className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
-                >
-                  Yearly
-                </button>
+                {getYearsArray()?.map((year, key) => {
+                  return (
+                    <button
+                      key={key}
+                      onClick={() => {
+                        setSelectedYear(year);
+                      }}
+                      className="font-medium text-[11px] w-full px-3 py-1 text-start hover:bg-gray-100"
+                    >
+                      {year}
+                    </button>
+                  );
+                })}
               </div>
             )}
           </div>
